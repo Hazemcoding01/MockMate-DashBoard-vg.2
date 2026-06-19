be0d27dfa7d358a82127c4388b4101d1a41e2769
Hazemcoding01 <ethhazem@gmail.com>
2026-06-20 01:47:38 +0300

    server changes

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   css: {
     transformer: 'postcss',
     minify: false
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://mockmateai-001-site1.jtempurl.com',
+        changeOrigin: true
+      }
+    }
   }
 });
